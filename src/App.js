785bd94321c983ab5bba381db53f8bf33173ee2d
Anurag Pandey <108535343+anurag-p15@users.noreply.github.com>
2025-08-06 23:00:51 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://i.postimg.cc/ydRbg8cZ/IMG-0536.png" className="App-logo" alt="logo" />
+        <p>
+          Launching Soon !!!.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
